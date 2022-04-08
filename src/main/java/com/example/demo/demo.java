f9c29d4449bcea162d60a26f3b18ea92842ca9a6
Haodong.guo <610880737@qq.com>
2022-04-08 15:54:42 +0800

    2

--- src/main/java/com/example/demo/demo.java
+++ src/main/java/com/example/demo/demo.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("this is a test for master");
+        System.out.println("this is a test for master..");
     }
 }
