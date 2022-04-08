2e60a39f334dea45e9fd867645a2d0f43d1abb3e
Haodong.guo <610880737@qq.com>
2022-04-08 13:55:04 +0800

    another terminal

--- src/main/java/com/example/demo/demo.java
+++ src/main/java/com/example/demo/demo.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("this is a test");
+        System.out.println("this is a test for dev");
     }
 }
